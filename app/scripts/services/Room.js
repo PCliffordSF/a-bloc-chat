b996100dc497beda79c8bee5ea6b44deb51ffbaa
Paul Clifford <paulclifford2570@gmail.com>
2016-11-08 19:45:19 +0000

    Monday Call with Richard

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,8 +1,10 @@
 (function() {
   function Room($firebaseArray) {
-      
-    var ref = firebase.database().ref().child("rooms");
-     var rooms = $firebaseArray(ref);
+      var firebaseRef = new Firebase('ablocchat.firebaseio.com');
+      console.log(firebaseRef);
+    // var ref = firebaseRef.database().ref().child("rooms");
+    var rooms = $firebaseArray(firebaseRef.child('rooms'));
+    // var rooms = ["livingroom", "bedroom"];
     
     return {
         all: rooms
